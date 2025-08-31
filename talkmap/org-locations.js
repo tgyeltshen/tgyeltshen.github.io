383cf8861005fc0da22d86a7f0c73709cbac0dee
Tshewang Gyeltshen <tgyeltshen09@gmail.com>
2025-08-31 19:00:26 +0900

    reverted talk.js

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,27 +1,22 @@
 var addressPoints = [
   [
-    "Lung Cancer Mortality and Air Pollution in Japan (scheduled) <br />Hitotsubashi Institute of Advanced Studies; Tokyo, Japan",
-    35.6895, // latitude Tokyo
-    139.6917 // longitude Tokyo
+    "Talk 2 on Relevant Topic in Your Field<br />London School of Testing; London, UK",
+    51.4893335,
+    -0.14405508452768728
   ],
   [
-    "Data Analysis using Comprehensive Survey of Living Conditions Survey, Japan<br />Kagoshima Data Science Symposium; Kagoshima, Japan",
-    31.5966, // latitude Kagoshima
-    130.5571 // longitude Kagoshima
+    "Tutorial 1 on Relevant Topic in Your Field<br />UC-Berkeley Institute for Testing Science; Berkeley, CA, USA",
+    37.8708393,
+    -122.272863
   ],
   [
-    "Intergenerational Inequities in Health Burden due to Climate Change <br />Utokyo Center for Climate Solutions, UTCCS Annual Workshop; The University of Tokyo, Japan",
-    35.7133, // latitude Bunkyo-ku (UTokyo Hongo campus)
-    139.7620 // longitude
+    "Conference Proceeding talk 3 on Relevant Topic in Your Field<br />Testing Institute of America 2014 Annual Conference; Los Angeles, CA, USA",
+    34.0536909,
+    -118.242766
   ],
   [
-    "Socioeconomic Inequalities in Uptake of Cancer Screening 2013–2022, Japan <br />The 84th Annual Meeting of the Japanese Society of Public Health; Sapporo, Japan",
-    43.0621, // latitude Sapporo
-    141.3544 // longitude
+    "Talk 1 on Relevant Topic in Your Field<br />UC San Francisco, Department of Testing; San Francisco, CA, USA",
+    37.7792588,
+    -122.4193286
   ]
 ];
-var map = L.map('map').setView([20, 0], 2);
-
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);  
